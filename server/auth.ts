a2235890c16d65ab6ea6841daea5d3a8ab52f793
Jackson Stone <jacksonstone@jacksons-mini-2.lan>
2024-07-04 09:04:26 -0500

    Correct key formatting

--- server/auth.ts
+++ server/auth.ts
@@ -1,12 +1,16 @@
 
 import { JaxAuth, JaxAuthBuilder } from "./JaxAuth";
 import { User } from "./types";
+import { Buffer } from 'buffer';
+
 
 const auth: JaxAuth<User> = new JaxAuthBuilder<User>()
     .setUserPasswordSaltField(user => user.salt)
     .setUserHashPasswordField(user => user.hashedPassword)
-    .setHMACKey(process.env.HMACKey as string)
-    .setEncryptionSecret(process.env.ENCRYPTION_KEY as string)
+    // HMACKey and EncryptionKey are stored in hex in the environment variables
+    // EncryptionKey is 16 bytes long, HMACKey is 32 bytes long
+    .setHMACKey(Buffer.from(process.env.HMAC_KEY as string, 'hex').toString('utf16le'))
+    .setEncryptionSecret(Buffer.from(process.env.ENCRYPTION_KEY as string, 'hex').toString('utf16le'))
     .setStringifiedCookieContents(user => JSON.stringify({userId: user.userId, userEmail: user.userEmail}))
     .setCookieName('auth')
     .setUseDevelopmentCookie(process.env.NODE_ENV !== "production")
