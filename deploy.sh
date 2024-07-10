32df092eeba0b6ebe0a52a13abec51d764bd55d6
Jackson Stone <jacksonastone@gmail.com>
2024-07-10 06:13:42 -0500

    Small style tweaks

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 npm run build
-zip -r -X theologian_chat.zip server-build
+zip -r -q -X theologian_chat.zip server-build
 scp -i /Users/jacksonstone/Desktop/Jackson\ Personal\ Site\ Key.pem theologian_chat.zip ubuntu@3.19.146.227:/home/ubuntu/.temp/
 ssh -i /Users/jacksonstone/Desktop/Jackson\ Personal\ Site\ Key.pem ubuntu@3.19.146.227 << EOF
   mv ./.temp/theologian_chat.zip . || { echo "Failed to move the file"; exit 1; }
-  rm -rf theologian_chat
-  unzip theologian_chat.zip -d theologian_chat
-  rm theologian_chat.zip
-  sudo systemctl restart theologian_chat
+  rm -rf theologian_chat || { echo "Failed to remove old files"; exit 1; }
+  unzip -q theologian_chat.zip -d theologian_chat || { echo "Failed to unzip new files"; exit 1; }
+  rm theologian_chat.zip 
+  sudo systemctl restart theologian_chat || { echo "Failed to restart service"; exit 1; }
 EOF
 rm theologian_chat.zip
+echo "Script completed successfully."
